6f8fcae3562af3b2371aa8b0f2cee5a4d2a42ffb
camilagsoares <camila.soares19@hotmail.com>
2024-04-29 19:21:17 -0300

    .

--- digimon-front/src/app/header/header.component.ts
+++ digimon-front/src/app/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../login/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FormsModule, CommonModule,],
+  imports: [FormsModule, CommonModule,RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
