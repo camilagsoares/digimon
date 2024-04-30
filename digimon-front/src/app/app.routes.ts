c6661e314bfae99f58d3f7a007eeb94f248ad257
camilagsoares <camila.soares19@hotmail.com>
2024-04-29 22:38:29 -0300

    modal

--- digimon-front/src/app/app.routes.ts
+++ digimon-front/src/app/app.routes.ts
@@ -18,9 +18,8 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [
-      RouterModule.forRoot(routes) 
-    ],
+  imports: [RouterModule.forRoot(routes)],
+
     
   })
   export class AppModule { }
